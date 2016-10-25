6bd7a165e8b2af426dff3e422bc770e7a9d5c68d
root <root@beaglebone.localdomain>
2016-10-25 04:50:34 +0000

    adding more files

--- AWS_IoT_Demo.py
+++ AWS_IoT_Demo.py
@@ -6,6 +6,7 @@ import grove_temperature_sensor
 import grove_oled
 import Adafruit_BBIO.GPIO as GPIO
 import random
+from adxl345 import ADXL345
 
 #dweetIO = "https://dweet.io/dweet/for/"
 #myName = "BBG_IoT_Demo"
@@ -22,15 +23,21 @@ if __name__=="__main__":
     grove_oled.oled_setNormalDisplay()
     grove_oled.oled_clearDisplay()
     while True:
-        # GPIO.output(led,GPIO.HIGH)
-        # time.sleep(1)
-        # GPIO.output(led,GPIO.LOW)
-        # time.sleep(1)
-        # print 'led'
+        
+        #3 Axis
+        adxl345 = ADXL345()
+        axes = adxl345.getAxes(True)
+        xVal = axes['x']
+        yVal = axes['y']
+        zVal = axes['z']
+        #print "x = %.3fG" % ( xVal )
         temperature = grove_temperature_sensor.read_temperature()
         temp_f = temperature * 9.0 / 5.0 + 32.0
+        #Placeholders-calculated values
         rpm = random.gauss (5000,50)
         fuellevel= random.gauss (3,.1)
+	
+        #Screen Controls
         grove_oled.oled_setTextXY(0,0)
         grove_oled.oled_putString("AWS IoT OSMC")
         grove_oled.oled_setTextXY(1,0)
@@ -41,19 +48,15 @@ if __name__=="__main__":
         grove_oled.oled_putString('RPM:{0:0.1f} '.format(rpm))
         grove_oled.oled_setTextXY(4,0)
         grove_oled.oled_putString('Fuel:{0:0.1f}'.format(fuellevel))
+        grove_oled.oled_setTextXY(5,0)
+        grove_oled.oled_putString('Lean X:{0:0.1f}'.format(xVal))
         grove_oled.oled_setTextXY(6,0)
-        grove_oled.oled_putString("AWS IoT")
-
+        grove_oled.oled_putString('Lean Y:{0:0.1f}'.format(yVal))
+        grove_oled.oled_setTextXY(7,0)
+        grove_oled.oled_putString('Lean Z:{0:0.1f}'.format(zVal))
+        #Show something to the console
         print 'Temp \n {0:0.2f} *C'.format(temp_f)
-
-        #rqsString = dweetIO + myName + '?' + myKey + '=' + '{0:0.1f} '.format(temperature)
-        # +'/' +  myKey + '=' + '{0:0.1f} *C'.format(sensor.read_temperature())
-        # rqsString = dweetIO + myName + '?' + "{'Temperatuer':'23C','ADC':'123'}"
-        #print rqsString
-        #rqs = requests.get(rqsString)
-        #print rqs.status_code
-        #print rqs.headers
-        #print rqs.content
+        #Check for High Temperature
         if temperature >= THRESHOLD_TEMPERATURE :
             GPIO.output(Buzzer,GPIO.HIGH)
         else:
